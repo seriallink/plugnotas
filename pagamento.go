3eeae818efcb3a78388d1a178b9dd67d1854541d
Marcelo Monaco <marcelo.monaco@gmail.com>
2021-08-04 17:12:12 -0300

    Adicionado campo DescricaoMeio

--- pagamento.go
+++ pagamento.go
@@ -1,8 +1,9 @@
 package plugnotas
 
 type Pagamento struct {
-	AVista *bool    `json:"aVista,omitempty"`
-	Meio   *string  `json:"meio,omitempty"`
-	Valor  *float64 `json:"valor,omitempty"`
-	Cartao *Cartao  `json:"cartao,omitempty"`
+	AVista        *bool    `json:"aVista,omitempty"`
+	Meio          *string  `json:"meio,omitempty"`
+	DescricaoMeio *string  `json:"descricaoMeio,omitempty"`
+	Valor         *float64 `json:"valor,omitempty"`
+	Cartao        *Cartao  `json:"cartao,omitempty"`
 }
