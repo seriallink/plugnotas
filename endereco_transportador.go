1b2ffd1c5bc11a4ce227e47a9dde8f31f0a9b4c1
Marcelo Monaco <marcelo.monaco@gmail.com>
2022-04-27 09:47:24 -0300

    Adicionado uf no endereço do transportador

--- endereco_transportador.go
+++ endereco_transportador.go
@@ -3,4 +3,5 @@ package plugnotas
 type EnderecoTransportador struct {
 	Logradouro      *string `json:"logradouro,omitempty"`
 	DescricaoCidade *string `json:"descricaoCidade,omitempty"`
+	UF              *string `json:"uf,omitempty"`
 }
